598b5411427d13d6d08909ba95055a6a88c0f48c
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-23 01:34:37 +0530

    axios with userEffect ,async function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [data,setData]=useState();
-  axios
-  .get('https://api.restful-api.dev/objects')
-  .then((response)=>{
-    setData(response.data[0].name);
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
+  // const [data,setData]=useState();
+  // axios
+  // .get('https://api.restful-api.dev/objects')
+  // .then((response)=>{
+  //   setData(response.data[0].name);
+  // })
+  // .catch((error)=>{
+  //   console.log(error)
+  // })
+const [deviceName,setDeviceName]=useState();
+const [deviceColor,setDeviceColor]=useState();
+const [deviceCapacity,setDeviceCapacity]=useState();
+
+const url='https://api.restful-api.dev/objects';
+
+useEffect(()=>{
+   const fetchData=async ()=>{
+    try {
+      const result=await axios.get(url);
+      const needData=result.data[2];
+ 
+      setDeviceName(needData.name);
+      setDeviceColor(needData.data.color);
+      setDeviceCapacity(needData.data['capacity GB']);//keywise accesed because space have betwenn variable
+ 
+    } catch (error) {
+      console.log(error);
+    }
+   }
+   fetchData();
+},[])
+
+
+
   return (
     <div className='background'>
-      <div className='from-title'>{data}</div>
+      <div className='device'>{deviceName}</div>
+      <div className='device-color'>{deviceColor}</div>
+      <div className='device-capcity'>{deviceCapacity}</div>
     </div>
   );
 }
