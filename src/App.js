ac9bf9780c01670f30a98345b14c5cb8cf9665bc
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-24 12:14:44 +0530

    post request

--- src/App.js
+++ src/App.js
@@ -13,36 +13,97 @@ function App() {
   // .catch((error)=>{
   //   console.log(error)
   // })
-const [deviceName,setDeviceName]=useState();
-const [deviceColor,setDeviceColor]=useState();
-const [deviceCapacity,setDeviceCapacity]=useState();
-
+const [deviceName,setDeviceName]=useState('');
+const [deviceYear,setDeviceYear]=useState('');
+const [devicePrice,setDevicePrice]=useState('');
+//get url
 const url='https://api.restful-api.dev/objects';
 
-useEffect(()=>{
-   const fetchData=async ()=>{
+//add object
+const url_2='https://api.restful-api.dev/objects';
+
+// useEffect(()=>{
+//    const fetchData=async ()=>{
+//     try {
+//       const result=await axios.get(url);
+//       const needData=result.data[2];
+ 
+//       setDeviceName(needData.name);
+//       setDeviceColor(needData.data.color);
+//       setDeviceCapacity(needData.data['capacity GB']);//keywise accesed because space have betwenn variable
+ 
+//     } catch (error) {
+//       console.log(error);
+//     }
+//    }
+//    fetchData();
+// },[])
+    
+const handleSubmit= async(event)=>{
+  event.preventDefault();
     try {
-      const result=await axios.get(url);
-      const needData=result.data[2];
- 
-      setDeviceName(needData.name);
-      setDeviceColor(needData.data.color);
-      setDeviceCapacity(needData.data['capacity GB']);//keywise accesed because space have betwenn variable
- 
+       const submitData={
+         name:deviceName,
+         data:{
+          year:deviceYear,
+          price:devicePrice,
+         }
+       } ;
+       const response=await axios.post(url_2,submitData);
+       console.log(response);
     } catch (error) {
       console.log(error);
     }
-   }
-   fetchData();
-},[])
-
+}
 
 
   return (
+    // <div className='background'>
+    //   <div className='device'>{deviceName}</div>
+    //   <div className='device-color'>{deviceColor}</div>
+    //   <div className='device-capcity'>{deviceCapacity}</div>
+    // </div>
     <div className='background'>
-      <div className='device'>{deviceName}</div>
-      <div className='device-color'>{deviceColor}</div>
-      <div className='device-capcity'>{deviceCapacity}</div>
+      <form action='' onSubmit={handleSubmit}>
+          <p>Submit Your Device</p>
+          <div>Device Name</div>
+          <input 
+            type="text" 
+            name='deviceName' 
+            placeholder='Enter Device Name'
+            value={deviceName}
+            onChange={(inputDeviceName)=>{
+              setDeviceName(inputDeviceName.target.value);
+            }}
+            
+           />
+           <div>Device Year</div>
+          <input 
+            type="text" 
+            name='deviceYear' 
+            placeholder='Enter Device year'
+            value={deviceYear}
+            onChange={(inputDeviceYear)=>{
+              setDeviceYear(inputDeviceYear.target.value);
+            }}
+            
+           />
+           <div>Device Price</div>
+          <input 
+            type="text" 
+            name='deviceYear' 
+            placeholder='Enter Device year'
+            value={devicePrice}
+            onChange={(inputDevicePrice)=>{
+              setDevicePrice(inputDevicePrice.target.value)
+            }}
+            
+           />
+           <br />
+           <button type='submit'>Submit your Device</button>
+           
+      </form>
+
     </div>
   );
 }
